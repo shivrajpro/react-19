42fad7ceb327486ce3435275d81a124118f9aa9f
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-01-17 00:49:00 +0530

    two data binding

---
+++ src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    // setIsEditing(!isEditing); //avoid this practice
+    setIsEditing((editing) => !editing);
+  }
+
+  function playerNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {isEditing && (
+          <input
+            className="player-name"
+            value={playerName}
+            onChange={playerNameChange}
+          />
+        )}
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
