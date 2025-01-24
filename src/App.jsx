ef3ba0f69c50f42097ebfa7a3ce1a15380f2fbcf
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-01-24 20:47:27 +0530

    getting started with project management app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import Player from "./components/Player.jsx";
-import TimerChallenge from "./components/TimerChallenge.jsx";
-
 function App() {
   return (
     <>
-      <Player />
-      <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={1} />
-        <TimerChallenge title="Not Easy" targetTime={5} />
-        <TimerChallenge title="Getting Tough" targetTime={10} />
-        <TimerChallenge title="Pros Only" targetTime={15} />
-      </div>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
 }
