9dfad71ed3c7eb274887562b179de018e878f5eb
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2025-02-12 23:09:19 +0530

    passing events as props in react

--- react-thapa/src/App.jsx
+++ react-thapa/src/App.jsx
@@ -1,14 +1,18 @@
-import NetflixSeries from "./components/NetflixSeries";
+// import EventHandling from "./components/EventHandling";
+import { EventProps } from "./components/EventProps";
+// import NetflixSeries from "./components/NetflixSeries";
 // import Profile from "./components/Profile";
 // import "./components/NetflixSeries.css";
 
 export const App = () => {
   return (
     <section className="container">
+      {/* <EventHandling /> */}
+      <EventProps />
       {/* <Header /> */}
       {/* <Profile /> */}
-      <h1 className="card-heading">List of Best Netflix Series</h1>
-      <NetflixSeries />
+      {/* <h1 className="card-heading">List of Best Netflix Series</h1> */}
+      {/* <NetflixSeries /> */}
       {/* <Footer /> */}
     </section>
   );
